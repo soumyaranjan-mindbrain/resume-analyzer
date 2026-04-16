5c358cf5a8df2990d9667f38679669bf73383963
soumya ranjan <soumyaranjanpanigrahi111@gmail.com>
2026-04-16 12:57:21 +0530

    feat: implement MongoDB connection configuration using mongoose

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);  
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected Successfully!!');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
